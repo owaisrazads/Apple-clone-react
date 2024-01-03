49800a32d14d1f163e6684b71b161ed18a5ceff8
unknown <oraza7969@gmail.com>
2024-01-03 10:16:48 -0800

    Apple clone react

---
+++ Apple clone react/src/App.jsx
@@ -0,0 +1,41 @@
+
+import img1 from './image/2024.jpg'
+
+import './App.css'
+
+function App() {
+  
+
+
+  return (
+    <>
+     <nav class="bg-[#404040] p-4">
+      <div class="container mx-auto flex justify-between items-center">
+      <div class="text-white text-3xl font-bold"><i class="fa-brands fa-apple"></i></div>
+      <div class="flex space-x-4">
+      <a href="#" class="text-white hover:text-gray-300">Store</a>
+      <a href="#" class="text-white hover:text-gray-300">Mac</a>
+      <a href="#" class="text-white hover:text-gray-300">ipad</a>
+      <a href="#" class="text-white hover:text-gray-300">Watch</a>
+      <a href="#" class="text-white hover:text-gray-300">Vision</a>
+      <a href="#" class="text-white hover:text-gray-300">ConAirpodstact</a>
+      <a href="#" class="text-white hover:text-gray-300">TV & Home</a>
+      <a href="#" class="text-white hover:text-gray-300">Entertaintment</a>
+      <a href="#" class="text-white hover:text-gray-300">Accersories</a>
+      <a href="#" class="text-white hover:text-gray-300">Support</a>
+      <a href="#" class="text-white hover:text-gray-300"><i class="fa-solid fa-magnifying-glass"></i></a>
+      <a href="#" class="text-white hover:text-gray-300"><i class="fa-solid fa-bag-shopping"></i></a>
+    </div>
+   </div>
+  </nav>
+  <head>
+<div ><img src={img1} alt="" /></div>
+
+  
+</head>
+    
+    </>
+  )
+}
+
+export default App
